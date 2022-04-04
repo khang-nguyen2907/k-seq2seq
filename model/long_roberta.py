1505f283be5cbf25d4f213e40b8104e57cea8924
khang-nguyen2907 <74083431+khang-nguyen2907@users.noreply.github.com>
2022-04-04 21:37:20 +0700

    Update long_roberta.py

--- model/long_roberta.py
+++ model/long_roberta.py
@@ -4,7 +4,7 @@ import math
 import copy
 import torch
 from dataclasses import dataclass, field
-from transformers import RobertaForMaskedLM, RobertaTokenizerFast, TextDataset, DataCollatorForLanguageModeling, Trainer
+from transformers import RobertaForMaskedLM, RobertaTokenizerFast, TextDataset, DataCollatorForLanguageModeling, Trainer, RobertaModel
 from transformers import TrainingArguments, HfArgumentParser
 from transformers import LongformerSelfAttention
 
@@ -27,37 +27,37 @@ class RobertaLongSelfAttention(LongformerSelfAttention):
 class RobertaLongForMaskedLM(RobertaForMaskedLM):
     def __init__(self, config):
         super().__init__(config)
-        for i, layer in enumerate(self.roberta.encoder.layer):
+        for i, layer in enumerate(self.encoder.layer):
             # replace the `modeling_bert.BertSelfAttention` object with `LongformerSelfAttention`
             layer.attention.self = RobertaLongSelfAttention(config, layer_id=i)
 
 
 def create_long_model(save_model_to, attention_window, max_pos):
-    model = RobertaForMaskedLM.from_pretrained('roberta-base')
-    tokenizer = RobertaTokenizerFast.from_pretrained('roberta-base', model_max_length=max_pos)
+    model = RobertaForMaskedLM.from_pretrained('distilroberta-base')
+    tokenizer = RobertaTokenizerFast.from_pretrained('distilroberta-base', model_max_length=max_pos)
     config = model.config
 
     # extend position embeddings
     tokenizer.model_max_length = max_pos
     tokenizer.init_kwargs['model_max_length'] = max_pos
-    current_max_pos, embed_size = model.roberta.embeddings.position_embeddings.weight.shape
+    current_max_pos, embed_size = model.embeddings.position_embeddings.weight.shape
     max_pos += 2  # NOTE: RoBERTa has positions 0,1 reserved, so embedding size is max position + 2
     config.max_position_embeddings = max_pos
     assert max_pos > current_max_pos
     # allocate a larger position embedding matrix
-    new_pos_embed = model.roberta.embeddings.position_embeddings.weight.new_empty(max_pos, embed_size)
+    new_pos_embed = model.embeddings.position_embeddings.weight.new_empty(max_pos, embed_size)
     # copy position embeddings over and over to initialize the new position embeddings
     k = 2
     step = current_max_pos - 2
     while k < max_pos - 1:
-        new_pos_embed[k:(k + step)] = model.roberta.embeddings.position_embeddings.weight[2:]
+        new_pos_embed[k:(k + step)] = model.embeddings.position_embeddings.weight[2:]
         k += step
-    model.roberta.embeddings.position_embeddings.weight.data = new_pos_embed
-    model.roberta.embeddings.position_ids.data = torch.tensor([i for i in range(max_pos)]).reshape(1, max_pos)
+    model.embeddings.position_embeddings.weight.data = new_pos_embed
+    model.embeddings.position_ids.data = torch.tensor([i for i in range(max_pos)]).reshape(1, max_pos)
 
     # replace the `modeling_bert.BertSelfAttention` object with `LongformerSelfAttention`
     config.attention_window = [attention_window] * config.num_hidden_layers
-    for i, layer in enumerate(model.roberta.encoder.layer):
+    for i, layer in enumerate(model.encoder.layer):
         longformer_self_attn = LongformerSelfAttention(config, layer_id=i)
         longformer_self_attn.query = layer.attention.self.query
         longformer_self_attn.key = layer.attention.self.key
@@ -75,7 +75,7 @@ def create_long_model(save_model_to, attention_window, max_pos):
     return model, tokenizer
 
 def copy_proj_layers(model):
-    for i, layer in enumerate(model.roberta.encoder.layer):
+    for i, layer in enumerate(model.encoder.layer):
         layer.attention.self.query_global = copy.deepcopy(layer.attention.self.query)
         layer.attention.self.key_global = copy.deepcopy(layer.attention.self.key)
         layer.attention.self.value_global = copy.deepcopy(layer.attention.self.value)
